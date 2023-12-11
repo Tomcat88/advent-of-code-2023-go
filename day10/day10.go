0187c3a937463999ec9a0ea5fc5dce898b796d44
Thomas Introini <thomas.introini@gmail.com>
2023-12-11 22:18:39 +0100

    :sparkles: add day 10 part 2

--- day10/day10.go
+++ day10/day10.go
@@ -8,12 +8,11 @@ import (
 
 func main() {
 	lines, err := utils.GetInput("day10")
-	// lines, err := utils.GetInput("day10")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("part 1:", part1(lines))
-	// fmt.Println("part 2:", part2(lines))
+	fmt.Println("part 2:", part2(lines))
 }
 
 func part1(lines []string) int {
@@ -28,7 +27,6 @@ func part1(lines []string) int {
 	currents := [][2]int{
 		{startI, startJ},
 	}
-	// startString := joinToString(currents[0], ",")
 	visited := map[[2]int]int{currents[0]: 0}
 	steps := 0
 	for len(currents) > 0 {
@@ -57,14 +55,7 @@ func part1(lines []string) int {
 	return max
 }
 
-func inside(lines []string, boundaries map[[2]int]int, i, j int, boundaryCount int) bool {
-	current := lines[i][j]
-	_, isBoundary := boundaries[[2]int{i, j}]
-	fmt.Println(current, i, j, isBoundary, boundaryCount)
-	return false
-}
-
-func prettyPrint(lines []string, boundaries map[[2]int]int, outside map[[2]int]int, replace bool) {
+func prettyPrint(lines []string, boundaries map[[2]int]int, inside map[[2]int]int, replace bool) {
 	for i, line := range lines {
 		for j, r := range line {
 			if _, found := boundaries[[2]int{i, j}]; found {
@@ -73,10 +64,10 @@ func prettyPrint(lines []string, boundaries map[[2]int]int, outside map[[2]int]i
 				} else {
 					fmt.Print(string(r))
 				}
-			} else if _, found := outside[[2]int{i, j}]; found {
-				fmt.Print("O")
+			} else if _, found := inside[[2]int{i, j}]; found {
+				fmt.Print("I")
 			} else {
-				fmt.Print("X")
+				fmt.Print("O")
 			}
 		}
 		fmt.Println()
@@ -108,39 +99,11 @@ var moves = [][2]int{
 	{1, 0},  // Down
 }
 
-var squeezeAllowedPipes = map[[2]int][]rune{
-	Right: []rune{'-', 'J', '7'},
-	Left:  []rune{'-', 'F', 'L'},
-	Up:    []rune{'|', 'F', '7'},
-	Down:  []rune{'|', 'J', 'L'},
-}
-
-func allowedOutsiseMoves(lines []string, boundaries map[[2]int]int, i, j int) (allowed [][2]int) {
-	// currentPosition := lines[i][j]
-	for _, move := range moves {
-		newI, newJ := i+move[0], j+move[1]
-		if newI < 0 || newJ < 0 {
-			continue
-		}
-		if newI >= len(lines) {
-			continue
-		}
-		if newJ >= len(lines[0]) { // Every line should have the same length
-			continue
-		}
-		if boundaries[[2]int{newI, newJ}] > 0 {
-			continue
-		}
-		allowed = append(allowed, move)
-	}
-	return
-}
-
 var allowedPipes = map[[2]int][]rune{
-	Right: []rune{'-', 'J', '7'},
-	Left:  []rune{'-', 'F', 'L'},
-	Up:    []rune{'|', 'F', '7'},
-	Down:  []rune{'|', 'J', 'L'},
+	Right: {'-', 'J', '7'},
+	Left:  {'-', 'F', 'L'},
+	Up:    {'|', 'F', '7'},
+	Down:  {'|', 'J', 'L'},
 }
 
 func allowedMoves(lines []string, i, j int) (allowed [][2]int) {
@@ -160,37 +123,30 @@ func allowedMoves(lines []string, i, j int) (allowed [][2]int) {
 		switch currentPosition {
 		case 'S': // Allowed: Right: - J 7 Left: - F L Up: | F 7 Down: | J L
 			if slices.Contains(allowedPipes[move], newPosition) {
-				// fmt.Println(string(currentPosition), string(newPosition))
 				allowed = append(allowed, move)
 			}
 		case '-':
 			if (move == Right || move == Left) && slices.Contains(allowedPipes[move], newPosition) {
-				// fmt.Println(string(currentPosition), string(newPosition))
 				allowed = append(allowed, move)
 			}
 		case '|':
 			if (move == Up || move == Down) && slices.Contains(allowedPipes[move], newPosition) {
-				// fmt.Println(string(currentPosition), string(newPosition))
 				allowed = append(allowed, move)
 			}
 		case 'J':
 			if (move == Up || move == Left) && slices.Contains(allowedPipes[move], newPosition) {
-				// fmt.Println(string(currentPosition), string(newPosition))
 				allowed = append(allowed, move)
 			}
 		case '7':
 			if (move == Down || move == Left) && slices.Contains(allowedPipes[move], newPosition) {
-				// fmt.Println(string(currentPosition), string(newPosition))
 				allowed = append(allowed, move)
 			}
 		case 'F':
 			if (move == Down || move == Right) && slices.Contains(allowedPipes[move], newPosition) {
-				// fmt.Println(string(currentPosition), string(newPosition))
 				allowed = append(allowed, move)
 			}
 		case 'L':
 			if (move == Up || move == Right) && slices.Contains(allowedPipes[move], newPosition) {
-				// fmt.Println(string(currentPosition), string(newPosition))
 				allowed = append(allowed, move)
 			}
 		}
@@ -199,20 +155,11 @@ func allowedMoves(lines []string, i, j int) (allowed [][2]int) {
 	return allowed
 }
 
-/* func joinToString(ints [2]int, sep string) string {
-	slice := make([]string, len(ints))
-	for i, v := range ints {
-		slice[i] = strconv.Itoa(v)
-	}
-	return strings.Join(slice, sep)
-} */
-
 func part2(lines []string) int {
 	var startI, startJ int
 	for i, line := range lines {
 		for j, r := range line {
 			if r == 'S' {
-				fmt.Println("Starting from", i, j)
 				startI, startJ = i, j
 			}
 		}
@@ -220,7 +167,6 @@ func part2(lines []string) int {
 	currents := [][2]int{
 		{startI, startJ},
 	}
-	// startString := joinToString(currents[0], ",")
 	visited := map[[2]int]int{currents[0]: 0}
 	steps := 0
 	for len(currents) > 0 {
@@ -233,34 +179,28 @@ func part2(lines []string) int {
 				nextI, nextJ := i+move[0], j+move[1]
 				if _, found := visited[[2]int{nextI, nextJ}]; !found {
 					newCurrents = append(newCurrents, [2]int{nextI, nextJ})
-					// visited[] = 1
 				}
 			}
 		}
 		currents = newCurrents
 		steps++
 	}
-	fmt.Println("visited", visited)
-	outsideVisited := map[[2]int]int{{0, 0}: 1}
+	inside := map[[2]int]int{}
+	// prettyPrint(lines, visited, inside, true)
 	for i, line := range lines {
-		for j, _ := range line {
-			boundaryCount := 0
-			if inside(lines, visited, i, j, boundaryCount) {
-				outsideVisited[[2]int{i, j}] = 1
+		in := false
+		for j, rune := range line {
+			if _, found := visited[[2]int{i, j}]; found {
+				if rune == '|' || rune == 'L' || rune == 'J' {
+                    in = !in
+                }
+			} else {
+				if in {
+					inside[[2]int{i, j}] = 1
+				}
 			}
 		}
 	}
-	prettyPrint(lines, visited, outsideVisited, true)
-	/* boundaries := utils.Keys(visited)
-	 for len(currents) > 0 {
-		for _, current := range currents {
-			i, j := current[0], current[1]
-			if slices.Contains(boundaries, [2]int{i, j}) {
-				continue
-			}
-
-		}
-
-	} */
-	return 0
+	// prettyPrint(lines, visited, inside, true)
+	return len(inside)
 }
